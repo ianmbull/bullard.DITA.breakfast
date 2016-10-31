cff7ac7f934897ae1a4bc224d141038384e79acc
ianmbull <ianmbull@vt.edu>
2016-10-31 02:22:31 -0400

    upload2

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,5 +1,5 @@
 fil = new Array();
-fil["0"]= "Creating User Documentation/bullard.dita.breakfast1/References.html@@@References@@@Blueberry Oatmeal Waffles Recipe Breakfast Sandwich Jumbo Chickpea Pancake...";
-fil["1"]= "Creating User Documentation/bullard.dita.breakfast1/t-blueberry-oatmeal.html@@@How to Make Blueberry Oatmeal Waffles@@@Waffles are great to eat during any breakfast meal. Here is a fast and quick recipe to enjoying blueberry oatmeal waffles in the morning...";
-fil["2"]= "Creating User Documentation/bullard.dita.breakfast1/t-breakfast-sandwich.html@@@How to make a Breakfast Sandwich@@@Ingredients 1 English muffin 2 tablespoon soy vegan sausage, pressed into a round patty 3 thin slices of bell pepper 1 tablespoon vegan shredded cheese ½ teaspoon vegan butter pinch of black pepper...";
-fil["3"]= "Creating User Documentation/bullard.dita.breakfast1/t-chickpea.html@@@How to make Jumbo Chickpea Pancake@@@Ingredients 1 green onion, finely chopped (about ¼ cup) ¼ cup finely chopped red pepper ½ cup chickpea flour (also known as garbanzo flour or besan) ¼ teaspoon garlic powder ¼ teaspoon fine grain sea...";
+fil["0"]= "References.html@@@References@@@Blueberry Oatmeal Waffles Recipe Breakfast Sandwich Jumbo Chickpea Pancake Grilled Steak Recipe...";
+fil["1"]= "t-blueberry-oatmeal.html@@@How to Make Blueberry Oatmeal Waffles@@@Waffles are great to eat during any breakfast meal. Here is a fast and quick recipe to enjoying blueberry oatmeal waffles in the morning...";
+fil["2"]= "t-breakfast-sandwich.html@@@How to make a Breakfast Sandwich@@@A wonderful and nutritious breakfst sandwich to start your day off right...";
+fil["3"]= "t-chickpea.html@@@How to make Jumbo Chickpea Pancake@@@This meal is an interesting departure from the average pancake. This is a jumbo sized pancake with vegetables delicately placed on top for a familiar mealdone in a slightly different way...";
